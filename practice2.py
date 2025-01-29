d3d47825a765acde19509ffddd2364141d5c8de0
[arshiabyya] <abhatt21@ucsc.edu>
2025-01-28 18:54:16 -0800

    Halfway done editing file!

---
+++ practice2.py
@@ -0,0 +1,14 @@
+#first you check your git version in terminal with
+print ("git --version")
+#then make folder/directory with 
+print("mkdir {foldername}")
+#change path of terminal with
+print("cd {foldername}")
+#create code file with
+print("code {filename}")
+#initialize empty git repository in folder
+print("git init")
+#remember to save file in vscode before adding the file to the git with
+print("git add {filename}")
+#check status of git repository with
+print("git status")
